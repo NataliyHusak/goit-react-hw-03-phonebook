2c3727439e35f82a02cfeac4b1c7932a6db9dbf9
NataliyHusak <50527604+NataliyHusak@users.noreply.github.com>
2020-02-16 12:10:44 +0200

    Updates

---
+++ precache-manifest.70b117ac6df27e80752abcaf50918cf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292493677600b676b3707aa9d4a9d805",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "25d3ca2c46733b0c259d",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.b920e09f.chunk.css"
+  },
+  {
+    "revision": "28e408524e2cde331457",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b9666ecc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b9666ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d3ca2c46733b0c259d",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.b1353728.chunk.js"
+  },
+  {
+    "revision": "8b583280b34de9d01efa",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.8935b325.js"
+  }
+]);
